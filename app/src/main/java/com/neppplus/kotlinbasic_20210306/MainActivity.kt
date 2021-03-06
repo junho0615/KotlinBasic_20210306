4d304f35dff1a15febc7398d7780de1b46571b51
홍준호 <0615wnsgh@gmail.com>
2021-03-06 15:55:15 +0900

    테스트버튼 누르면 > 로그 출력

--- app/src/main/java/com/neppplus/kotlinbasic_20210306/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210306/MainActivity.kt
@@ -2,6 +2,8 @@ package com.neppplus.kotlinbasic_20210306
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        testBtn1.setOnClickListener {
+
+//            Ctrl + / : 코드에 메모 (주석) 남기기
+
+//            이 중괄호 안에 적는 코드가, testBtn1이 눌렸을 때 실행해줄 일.
+
+            Log.d("태그종류", "테스트1 버튼 눌림")
+
+        }
     }
 
 }
